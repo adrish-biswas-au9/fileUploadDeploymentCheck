36fa2700a2114713817a4dcffc158381f6ca068e
Adrish <adrishlinkin@gmail.com>
2021-02-22 11:48:54 +0530

    File upload Deployment Check

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fileupload= require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
 const mongo = require('mongodb');
 //const url="mongodb://localhost:27017"
 const url = "mongodb+srv://aRIMEHA_74:7447@cluster0.megge.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -8,11 +9,17 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 let dbo;
 let col_name = "files";
-
+cloudinary.config({
+    cloud_name: 'aryabhatta',
+    api_key: '346714616856731',
+    api_secret: 'YRCQnQAD3PmwdJ8kE3Wd3vVg_Qc'
+})
 //middleware
 //cross origin resource sharing
 app.use(cors())
-app.use(fileupload());
+app.use(fileupload({
+    useTempFiles: true
+}));
 //parse data for post call
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -44,23 +51,31 @@ app.get('/images', (req, res) => {
 app.post('/images_upload',(req,res)=>{
     console.log(req.files)
     console.log(req.body)
+    
     let image=req.files.avatar;
     if(image.mimetype!=='image/jpeg'){
         return res.redirect("/?errmessage=Only jpg/jpeg extensions are allowed.")
     }
-    image.mv(__dirname + '/public/images/'+ image.name,(err,data)=>{
-        if(err)  throw err;
+    cloudinary.uploader.upload(image.tempFilePath, (err, result)=> { 
+        if(err) throw err;
+        //return res.send(result)
         let info={
             user_given_name: req.body.user_name,
-            image_name: image.name
-        }
+            image_name: result.original_filename,
+            path: result.url
+        } 
         dbo.collection(col_name).insert(info, (err, data) => {
             if (err) throw err;
             return res.redirect("/?successmessage=Successfully Uploaded!")
             // res.status(200).send("Data Registered.")
         });
+    });
+    // image.mv(__dirname + '/public/images/'+ image.name,(err,data)=>{
+    //     if(err)  throw err;
+   
         
-    })
+        
+    // })
 })
 
 const mongodbClient = new mongo.MongoClient(url);
